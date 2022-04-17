96405a6db89ab1470b58c76804dd4afebce01eed
zprebo <zachprebo@gmail.com>
2022-04-17 04:36:26 +0100

    add .envs

--- pi_scripts/auto_shutdown_trigger.py
+++ pi_scripts/auto_shutdown_trigger.py
@@ -5,7 +5,8 @@ import os
 if __name__ == "__main__":
     headers = token_check()
     api_url = os.environ['API_URL']
-
+    print("Waiting for completed job...")
     job_completion = wait_for_done(api_url, headers)
     if job_completion == 100.0:
+        print("Job Complete. Triggering Alexa Routine...")
         trigger_alexa_routine()
